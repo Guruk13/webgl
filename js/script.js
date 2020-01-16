0a5c6b2d0bf3e14c4118b2a26e67354af3c994a9
marel <marwane.elaouni@gmail>
2020-01-16 18:55:18 +0100

    COmmit avant retour a méthode intersect sur groupes

--- js/script.js
+++ js/script.js
@@ -1,285 +1,137 @@
-import * as THREE from '../vendor/three.js-master/build/three.module.js';
-import Stats from '../vendor/three.js-master/examples/jsm/libs/stats.module.js';
-import { OrbitControls } from '../vendor/three.js-master/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from '../vendor/three.js-master/examples/jsm/loaders/FBXLoader.js';
 
-const Scene = {
-	vars: {
-		container: null,
-		scene: null,
-		records: null,
-		renderer: null,
-		camera: null,
-		stats: null,
-		controls: null,
-		texture: null,
-		mouse: new THREE.Vector2(),
-		INTERSECTED: null,
-		raycaster: new THREE.Raycaster(),
-		animSpeed: null,
-		animPercent: 0.00,
-		text: "DAWIN"
-	},
-	animate: () => {
-		Scene.vars.camera.updateMatrixWorld();
-		requestAnimationFrame(Scene.animate);
-		console.log(Scene.vars.records);
-		Scene.vars.raycaster.setFromCamera(Scene.vars.mouse, Scene.vars.camera);
+			import * as THREE from '../build/three.module.js';
 
-		Scene.customAnimation();
+			import Stats from './jsm/libs/stats.module.js';
 
-		let intersects = [];
+			var container, stats;
+			var camera, scene, raycaster, renderer;
 
+			var mouse = new THREE.Vector2(), INTERSECTED;
+			var radius = 100, theta = 0;
 
+			init();
+			animate();
 
-		if (intersects.length > 0) {
+			function init() {
 
-			if (Scene.vars.INTERSECTED != intersects[0].object) {
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
 
-				if (Scene.vars.INTERSECTED) Scene.vars.INTERSECTED.material.emissive.setHex(Scene.vars.INTERSECTED.currentHex);
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
 
-				Scene.vars.Scene.vars.INTERSECTED = intersects[0].object;
+				scene = new THREE.Scene();
+				scene.background = new THREE.Color( 0xf0f0f0 );
 
+				var light = new THREE.DirectionalLight( 0xffffff, 1 );
+				light.position.set( 1, 1, 1 ).normalize();
+				scene.add( light );
+
+				var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+
+				for ( var i = 0; i < 2000; i ++ ) {
+
+					var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+					object.position.x = Math.random() * 800 - 400;
+					object.position.y = Math.random() * 800 - 400;
+					object.position.z = Math.random() * 800 - 400;
+
+					object.rotation.x = Math.random() * 2 * Math.PI;
+					object.rotation.y = Math.random() * 2 * Math.PI;
+					object.rotation.z = Math.random() * 2 * Math.PI;
+
+					object.scale.x = Math.random() + 0.5;
+					object.scale.y = Math.random() + 0.5;
+					object.scale.z = Math.random() + 0.5;
+
+					scene.add( object );
+
+				}
+
+				raycaster = new THREE.Raycaster();
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				stats = new Stats();
+				container.appendChild( stats.dom );
+
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
 
 			}
 
-		} else {
-			if (Scene.vars.INTERSECTED) Scene.vars.INTERSECTED.material.emissive.setHex(Scene.vars.INTERSECTED.currentHex);
+			function onWindowResize() {
 
-			Scene.vars.INTERSECTED = null;
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
 
-		}
+				renderer.setSize( window.innerWidth, window.innerHeight );
 
+			}
 
+			function onDocumentMouseMove( event ) {
 
+				event.preventDefault();
 
-		Scene.vars.turn = null;
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-		if (intersects.length > 0) {
-			Scene.vars.turn = true;
-			Scene.vars.animSpeed = 0.05;
-		} else {
-			Scene.vars.turn = false;
-			Scene.vars.animSpeed = -0.05;
-		}
+			}
 
+			//
 
-		Scene.render();
-	},
-	render: () => {
-		Scene.vars.renderer.render(Scene.vars.scene, Scene.vars.camera);
-		Scene.vars.stats.update();
-	},
-	customAnimation: () => {
-		let vars = Scene.vars;
+			function animate() {
 
-		if (vars.animSpeed === null) {
-			return;
-		}
+				requestAnimationFrame( animate );
 
-		vars.animPercent = vars.animPercent + vars.animSpeed;
-		let percent = (vars.animPercent - 0.4) / 0.6;
+				render();
+				stats.update();
 
-		if (vars.turn && vars.Scene.vars.INTERSECTED != null) {
-			vars.Scene.vars.INTERSECTED.rotation.x = Math.PI / percent;
-		}
+			}
 
+			function render() {
 
+				theta += 0.1;
 
-		if (vars.animPercent >= 0.40) {
-			let percent = (vars.animPercent - 0.4) / 0.6;
-			//vars.statuette.position.y = 50 * percent;
+				camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
+				camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
+				camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
+				camera.lookAt( scene.position );
 
+				camera.updateMatrixWorld();
 
+				// find intersections
 
-		} else if (vars.animPercent < 0.70) {
-			//vars.statuette.position.y = 0;
-		}
+				raycaster.setFromCamera( mouse, camera );
 
+				var intersects = raycaster.intersectObjects( scene.children );
 
-	},
-	loadFBX: (file, scale, position, rotation, ) => {
-		let vars = Scene.vars;
-		let loader = new FBXLoader();
+				if ( intersects.length > 0 ) {
 
-		if (file === undefined) {
-			return;
-		}
+					if ( INTERSECTED != intersects[ 0 ].object ) {
 
-		let positionx = -500
-		for (let i = 0; i <= 2; i++) {
-			loader.load('./fbx/' + file, (object) => {
+						if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 
+						INTERSECTED = intersects[ 0 ].object;
+						INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+						INTERSECTED.material.emissive.setHex( 0xff0000 );
 
-				//console.log("loading captain")
-				position.x = positionx;
-				object.position.y = position[1];
-				object.position.z = position[2];
+					}
 
-				object.rotation.x = rotation[0];
-				object.rotation.y = rotation[1];
-				object.rotation.z = rotation[2];
+				} else {
 
-				object.scale.x = object.scale.y = object.scale.z = scale;
-				Scene.vars["record".$i] = mesh;
-				console.log("tell that bitch be cool ");
-				Scene.add(object);
+					if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
 
-			});
-			positionx += 500;
-		}
+					INTERSECTED = null;
 
+				}
 
+				renderer.render( scene, camera );
 
-
-
-
-
-
-	},
-
-
-	onWindowResize: () => {
-		let vars = Scene.vars;
-		vars.camera.aspect = window.innerWidth / window.innerHeight;
-		vars.camera.updateProjectionMatrix();
-		vars.renderer.setSize(window.innerWidth, window.innerHeight);
-	},
-	onMouseMove: (event) => {
-		Scene.vars.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-		Scene.vars.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-	},
-	init: () => {
-		let vars = Scene.vars;
-
-		// Préparer le container pour la scène
-		vars.container = document.createElement('div');
-		vars.container.classList.add('fullscreen');
-		document.body.appendChild(vars.container);
-
-		// ajout de la scène
-		vars.scene = new THREE.Scene();
-		vars.scene.background = new THREE.Color(0xa0a0a0);
-		vars.scene.fog = new THREE.Fog(vars.scene.background, 500, 3000);
-
-		// paramétrage du moteur de rendu
-		vars.renderer = new THREE.WebGLRenderer({ antialias: true });
-		vars.renderer.setPixelRatio(window.devicePixelRatio);
-		vars.renderer.setSize(window.innerWidth, window.innerHeight);
-
-		vars.renderer.shadowMap.enabled = true;
-		vars.renderer.shadowMapSoft = true;
-
-		vars.container.appendChild(vars.renderer.domElement);
-
-		// ajout de la caméra
-		vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-		vars.camera.position.set(50, 500, 1000);
-
-		// ajout de la lumière
-		const lightIntensityHemisphere = .5;
-		let light = new THREE.HemisphereLight(0xFFFFFF, 0x444444, lightIntensityHemisphere);
-		light.position.set(0, 700, 0);
-		vars.scene.add(light);
-
-		// ajout des directionelles
-		const lightIntensity = .8;
-		const d = 1000;
-		let light1 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
-		light1.position.set(0, 700, 0);
-		light1.castShadow = true;
-		light1.shadow.camera.left = -d;
-		light1.shadow.camera.right = d;
-		light1.shadow.camera.top = d;
-		light1.shadow.camera.bottom = -d;
-		light1.shadow.camera.far = 2000;
-		light1.shadow.mapSize.width = 4096;
-		light1.shadow.mapSize.height = 4096;
-		vars.scene.add(light1);
-		// let helper = new THREE.DirectionalLightHelper(light1, 5);
-		// vars.scene.add(helper);
-
-		let light2 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
-		light2.position.set(-400, 200, 400);
-		light2.castShadow = true;
-		light2.shadow.camera.left = -d;
-		light2.shadow.camera.right = d;
-		light2.shadow.camera.top = d;
-		light2.shadow.camera.bottom = -d;
-		light2.shadow.camera.far = 2000;
-		light2.shadow.mapSize.width = 4096;
-		light2.shadow.mapSize.height = 4096;
-		vars.scene.add(light2);
-		// let helper2 = new THREE.DirectionalLightHelper(light2, 5);
-		// vars.scene.add(helper2);
-
-		let light3 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
-		light3.position.set(400, 200, 400);
-		light3.castShadow = true;
-		light3.shadow.camera.left = -d;
-		light3.shadow.camera.right = d;
-		light3.shadow.camera.top = d;
-		light3.shadow.camera.bottom = -d;
-		light3.shadow.camera.far = 2000;
-		light3.shadow.mapSize.width = 4096;
-		light3.shadow.mapSize.height = 4096;
-		vars.scene.add(light3);
-		// let helper3 = new THREE.DirectionalLightHelper(light3, 5);
-		// vars.scene.add(helper3);
-
-		// ajout du sol
-		let mesh = new THREE.Mesh(
-			new THREE.PlaneBufferGeometry(2000, 2000),
-			new THREE.MeshLambertMaterial(
-				{ color: new THREE.Color(0x888888) }
-			)
-		);
-		mesh.rotation.x = -Math.PI / 2;
-		mesh.receiveShadow = false;
-		vars.scene.add(mesh);
-
-		let planeMaterial = new THREE.ShadowMaterial();
-		planeMaterial.opacity = 0.07;
-		let shadowPlane = new THREE.Mesh(
-			new THREE.PlaneBufferGeometry(2000, 2000),
-			planeMaterial);
-		shadowPlane.rotation.x = -Math.PI / 2;
-		shadowPlane.receiveShadow = true;
-
-		vars.scene.add(shadowPlane);
-
-		// ajout de la texture helper du sol
-		let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-		grid.material.opacity = 0.2;
-		grid.material.transparent = true;
-		vars.scene.add(grid);
-
-		vars.texture = new THREE.TextureLoader().load('./texture/marbre.jpg');
-
-		let hash = document.location.hash.substr(1);
-		if (hash.length !== 0) {
-			let text = hash.substring();
-			Scene.vars.text = decodeURI(text);
-		}
-
-		Scene.loadFBX("SM_VinylRecord.fbx", 10, [20, 20, 10], [0, 0, 0])
-
-		// ajout des controles
-		vars.controls = new OrbitControls(vars.camera, vars.renderer.domElement);
-
-		vars.controls.target.set(0, 100, 0);
-		vars.controls.update();
-
-		window.addEventListener('resize', Scene.onWindowResize, false);
-		window.addEventListener('mousemove', Scene.onMouseMove, false);
-
-		vars.stats = new Stats();
-		vars.container.appendChild(vars.stats.dom);
-
-		Scene.animate();
-	}
-};
-
-Scene.init();
-
-
+			}
