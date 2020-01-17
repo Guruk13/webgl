fa36e669fa4d36ee45754a7af3956b069a9dc575
marel <marwane.elaouni@gmail>
2020-01-17 01:26:17 +0100

    une musique marche en autoload

--- js/script.js
+++ js/script.js
@@ -1,137 +1,403 @@
+import * as THREE from '../vendor/three.js-master/build/three.module.js';
+import Stats from '../vendor/three.js-master/examples/jsm/libs/stats.module.js';
+import { OrbitControls } from '../vendor/three.js-master/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from '../vendor/three.js-master/examples/jsm/loaders/FBXLoader.js';
 
-			import * as THREE from '../build/three.module.js';
+const Scene = {
+	vars: {
+		container: null,
+		scene: null,
+		renderer: null,
+		camera: null,
+		stats: null,
+		controls: null,
+		texture: null,
+		mouse: new THREE.Vector2(),
+		raycaster: new THREE.Raycaster(),
+		animSpeed: null,
+		animPercent: 0.20,
+		text: "DAWIN",
+		listener: null, 
+		audioLoader: null,
+		sound: null,
+		audiofile: null,
+	},
+	animate: () => {
+		requestAnimationFrame(Scene.animate);
+		Scene.vars.raycaster.setFromCamera(Scene.vars.mouse, Scene.vars.camera);
 
-			import Stats from './jsm/libs/stats.module.js';
+		Scene.customAnimation();
 
-			var container, stats;
-			var camera, scene, raycaster, renderer;
+		if (Scene.vars.Album !== undefined) {
+			// utiliser optionalTarget : pour eviter ca 
+			let intersects = Scene.vars.raycaster.intersectObjects(Scene.vars.Album.children, true);
 
-			var mouse = new THREE.Vector2(), INTERSECTED;
-			var radius = 100, theta = 0;
+			//s:console.log(intersects);
 
-			init();
-			animate();
+			Scene.vars.turn = null;
 
-			function init() {
+			if (intersects.length > 0) {
+				Scene.vars.turn = true;
+				Scene.vars.Which = intersects[0].object;
+				/*
+				console.log(Scene.vars.record2.children[0]);
+				console.log('intersect 0th . object ')
+				console.log(intersects[0].object);
+				*/
+				Scene.vars.animSpeed = 0.05;
+			} else {
+				Scene.vars.turn = false;
+				Scene.vars.Which = null;
+				Scene.vars.animSpeed = -0.05;
+			}
 
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
 
-				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
 
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0xf0f0f0 );
+			// let mouse = new THREE.Vector3(Scene.vars.mouse.x, Scene.vars.mouse.y, 0);
+			// mouse.unproject(Scene.vars.camera);
 
-				var light = new THREE.DirectionalLight( 0xffffff, 1 );
-				light.position.set( 1, 1, 1 ).normalize();
-				scene.add( light );
+			// let ray = new THREE.Raycaster(Scene.vars.camera.position, mouse.sub(Scene.vars.camera.position).normalize()); 
+			// let intersects = ray.intersectObjects(Scene.vars.goldGroup.children, true);
+			// if(intersects.length > 0) {
+			// 	var arrow = new THREE.ArrowHelper(ray.ray.direction, ray.ray.origin, 1000, 0xFF00000);
+			// 	Scene.vars.scene.add(arrow);
+			// }
+		}
 
-				var geometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
+		Scene.render();
+	},
+	render: () => {
+		Scene.vars.renderer.render(Scene.vars.scene, Scene.vars.camera);
+		Scene.vars.stats.update();
+	},
 
-				for ( var i = 0; i < 2000; i ++ ) {
 
-					var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+	loadMusic: () => {
+		let vars = Scene.vars;
+		vars.audioLoader.load('albumfolder/moroder.ogg', function (buffer) {
+			vars.sound.setBuffer(buffer); 
+			vars.sound.setLoop(true);
+			vars.sound.setVolume(0.5); 
+			vars.sound.play();
+		});
+		console.log("hol horse your the worst ");
 
-					object.position.x = Math.random() * 800 - 400;
-					object.position.y = Math.random() * 800 - 400;
-					object.position.z = Math.random() * 800 - 400;
 
-					object.rotation.x = Math.random() * 2 * Math.PI;
-					object.rotation.y = Math.random() * 2 * Math.PI;
-					object.rotation.z = Math.random() * 2 * Math.PI;
 
-					object.scale.x = Math.random() + 0.5;
-					object.scale.y = Math.random() + 0.5;
-					object.scale.z = Math.random() + 0.5;
 
-					scene.add( object );
 
+
+
+
+	},
+	customAnimation: () => {
+		let vars = Scene.vars;
+
+		if (vars.animSpeed === null) {
+			return;
+		}
+
+		vars.animPercent = vars.animPercent + 0.5;
+		let percent = (vars.animPercent - 0.2) / 0.55;
+		if (vars.audiofile !== undefined) {
+			vars.audiofile = "";
+		}
+
+		if (vars.turn && vars.Which != null) {
+
+
+			vars.Which.rotation.x = Math.PI / percent;
+			if (vars.Which === Scene.vars.record1.children[0]) {
+				vars.audiofile = "albumfolder/moroder.ogg";
+				Scene.loadMusic();
+
+			}
+			if (vars.Which === Scene.vars.record2.children[0]) {
+				vars.audiofile = "Contact";
+			}
+			if (vars.Which === Scene.vars.record3.children[0]) {
+				vars.audiofile = "Touch";
+			}
+
+		} else {
+			vars.audiofile = "";
+
+		}
+		if (vars.turn) {
+			console.log("playing");
+
+		}
+		else {
+
+		}
+
+
+
+
+	},
+	loadFBX: (file, scale, position, rotation, color, namespace, callback) => {
+		let vars = Scene.vars;
+		let loader = new FBXLoader();
+
+		if (file === undefined) {
+			return;
+		}
+
+		loader.load('./fbx/' + file, (object) => {
+
+			object.traverse((child) => {
+				if (child.isMesh) {
+
+					child.castShadow = true;
+					child.receiveShadow = true;
+
+
+					child.material.color = new THREE.Color(color);
 				}
+			});
 
-				raycaster = new THREE.Raycaster();
+			object.position.x = position[0];
+			object.position.y = position[1];
+			object.position.z = position[2];
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
+			object.rotation.x = rotation[0];
+			object.rotation.y = rotation[1];
+			object.rotation.z = rotation[2];
 
-				stats = new Stats();
-				container.appendChild( stats.dom );
+			object.scale.x = object.scale.y = object.scale.z = scale;
+			Scene.vars[namespace] = object;
 
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+			callback();
+		});
 
-				//
+	},
+	loadText: (text, scale, position, rotation, color, namespace, callback) => {
+		let loader = new THREE.FontLoader();
 
-				window.addEventListener( 'resize', onWindowResize, false );
+		if (text === undefined || text === "") {
+			return;
+		}
 
-			}
 
-			function onWindowResize() {
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
 
-			}
+		loader.load('./vendor/three.js-master/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+			let geometry = new THREE.TextGeometry(text, {
+				font,
+				size: 1,
+				height: 0.1,
+				curveSegments: 1,
+				bevelEnabled: false
+			});
 
-			function onDocumentMouseMove( event ) {
+			geometry.computeBoundingBox();
+			let offset = geometry.boundingBox.getCenter().negate();
+			geometry.translate(offset.x, offset.y, offset.z);
 
-				event.preventDefault();
+			let material = new THREE.MeshBasicMaterial({
+				color: new THREE.Color(color)
+			});
 
-				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+			let mesh = new THREE.Mesh(geometry, material);
 
-			}
+			mesh.position.x = position[0];
+			mesh.position.y = position[1];
+			mesh.position.z = position[2];
 
-			//
+			mesh.rotation.x = rotation[0];
+			mesh.rotation.y = rotation[1];
+			mesh.rotation.z = rotation[2];
 
-			function animate() {
+			mesh.scale.x = mesh.scale.y = mesh.scale.z = scale;
 
-				requestAnimationFrame( animate );
+			Scene.vars[namespace] = mesh;
 
-				render();
-				stats.update();
+			callback();
+		});
+	},
+	onWindowResize: () => {
+		let vars = Scene.vars;
+		vars.camera.aspect = window.innerWidth / window.innerHeight;
+		vars.camera.updateProjectionMatrix();
+		vars.renderer.setSize(window.innerWidth, window.innerHeight);
+	},
+	onMouseMove: (event) => {
+		Scene.vars.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+		Scene.vars.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+	},
+	init: () => {
+		let vars = Scene.vars;
 
-			}
+		// Préparer le container pour la scène
+		vars.container = document.createElement('div');
+		vars.container.classList.add('fullscreen');
+		document.body.appendChild(vars.container);
 
-			function render() {
+		// ajout de la scène
+		vars.scene = new THREE.Scene();
+		vars.scene.background = new THREE.Color(0xa0a0a0);
+		vars.scene.fog = new THREE.Fog(vars.scene.background, 500, 3000);
 
-				theta += 0.1;
+		// paramétrage du moteur de rendu
+		vars.renderer = new THREE.WebGLRenderer({ antialias: true });
+		vars.renderer.setPixelRatio(window.devicePixelRatio);
+		vars.renderer.setSize(window.innerWidth, window.innerHeight);
 
-				camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
-				camera.lookAt( scene.position );
+		vars.renderer.shadowMap.enabled = true;
+		vars.renderer.shadowMapSoft = true;
 
-				camera.updateMatrixWorld();
+		vars.container.appendChild(vars.renderer.domElement);
 
-				// find intersections
+		// ajout de la caméra
+		vars.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+		vars.camera.position.set(50, 500, 1000);
 
-				raycaster.setFromCamera( mouse, camera );
+		// ajout de la lumière
+		const lightIntensityHemisphere = .5;
+		let light = new THREE.HemisphereLight(0xFFFFFF, 0x444444, lightIntensityHemisphere);
+		light.position.set(0, 700, 0);
+		vars.scene.add(light);
 
-				var intersects = raycaster.intersectObjects( scene.children );
+		// ajout des directionelles
+		const lightIntensity = .8;
+		const d = 1000;
+		let light1 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
+		light1.position.set(0, 700, 0);
+		light1.castShadow = true;
+		light1.shadow.camera.left = -d;
+		light1.shadow.camera.right = d;
+		light1.shadow.camera.top = d;
+		light1.shadow.camera.bottom = -d;
+		light1.shadow.camera.far = 2000;
+		light1.shadow.mapSize.width = 4096;
+		light1.shadow.mapSize.height = 4096;
+		vars.scene.add(light1);
+		// let helper = new THREE.DirectionalLightHelper(light1, 5);
+		// vars.scene.add(helper);
 
-				if ( intersects.length > 0 ) {
+		let light2 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
+		light2.position.set(-400, 200, 400);
+		light2.castShadow = true;
+		light2.shadow.camera.left = -d;
+		light2.shadow.camera.right = d;
+		light2.shadow.camera.top = d;
+		light2.shadow.camera.bottom = -d;
+		light2.shadow.camera.far = 2000;
+		light2.shadow.mapSize.width = 4096;
+		light2.shadow.mapSize.height = 4096;
+		vars.scene.add(light2);
+		// let helper2 = new THREE.DirectionalLightHelper(light2, 5);
+		// vars.scene.add(helper2);
 
-					if ( INTERSECTED != intersects[ 0 ].object ) {
+		let light3 = new THREE.DirectionalLight(0xFFFFFF, lightIntensity);
+		light3.position.set(400, 200, 400);
+		light3.castShadow = true;
+		light3.shadow.camera.left = -d;
+		light3.shadow.camera.right = d;
+		light3.shadow.camera.top = d;
+		light3.shadow.camera.bottom = -d;
+		light3.shadow.camera.far = 2000;
+		light3.shadow.mapSize.width = 4096;
+		light3.shadow.mapSize.height = 4096;
+		vars.scene.add(light3);
+		// let helper3 = new THREE.DirectionalLightHelper(light3, 5);
+		// vars.scene.add(helper3);
 
-						if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+		// ajout du sol
+		let mesh = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(2000, 2000),
+			new THREE.MeshLambertMaterial(
+				{ color: new THREE.Color(0x888888) }
+			)
+		);
+		mesh.rotation.x = -Math.PI / 2;
+		mesh.receiveShadow = false;
+		vars.scene.add(mesh);
 
-						INTERSECTED = intersects[ 0 ].object;
-						INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-						INTERSECTED.material.emissive.setHex( 0xff0000 );
+		let planeMaterial = new THREE.ShadowMaterial();
+		planeMaterial.opacity = 0.07;
+		let shadowPlane = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry(2000, 2000),
+			planeMaterial);
+		shadowPlane.rotation.x = -Math.PI / 2;
+		shadowPlane.receiveShadow = true;
 
-					}
+		vars.scene.add(shadowPlane);
 
-				} else {
+		// ajout de la texture helper du sol
+		let grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+		grid.material.opacity = 0.2;
+		grid.material.transparent = true;
+		vars.scene.add(grid);
 
-					if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+		vars.texture = new THREE.TextureLoader().load('./texture/marbre.jpg');
 
-					INTERSECTED = null;
+		let hash = document.location.hash.substr(1);
+		if (hash.length !== 0) {
+			let text = hash.substring();
+			Scene.vars.text = decodeURI(text);
+		}
 
-				}
+		Scene.loadFBX("Logo_Feelity.FBX", 10, [20, 20, 10], [0, 0, 0], 0xFFFFFF, 'logo', () => {
+			Scene.loadFBX("SM_VinylRecord.fbx", 10, [-500, 100, 0], [0, 0, Math.PI / 2], 0xFFFFFF, 'record1', () => {
+				Scene.loadFBX("Statuette.FBX", 10, [0, 0, 0], [0, 0, 0], 0xFFD700, 'statuette', () => {
+					Scene.loadFBX("SM_VinylRecord.fbx", 10, [0, 100, 0], [0, 0, Math.PI / 2], 0xFFFFFF, 'record2', () => {
+						Scene.loadFBX("SM_VinylRecord.fbx", 10, [500, 100, 0], [0, 0, Math.PI / 2], 0xFFFFFF, 'record3', () => {
+							Scene.loadFBX("Plaquette.FBX", 10, [0, 4, 45], [0, 0, 0], 0xFFFFFF, 'plaquette', () => {
+								Scene.loadText(Scene.vars.text, 10, [0, 23, 52], [0, 0, 0], 0x1A1A1A, "texte", () => {
 
-				renderer.render( scene, camera );
+									let vars = Scene.vars;
 
-			}
+									let gold = new THREE.Group();
+									gold.add(vars.statuette);
+
+									vars.scene.add(gold);
+									vars.goldGroup = gold;
+
+
+									let album = new THREE.Group();
+									album.add(vars.record1);
+									album.add(vars.record2);
+									album.add(vars.record3);
+									vars.scene.add(album);
+									vars.Album = album;
+
+
+									// create an AudioListener and add it to the camera
+									vars.listener = new THREE.AudioListener();
+									vars.camera.add(vars.listener);
+									// create a global audio source
+									vars.sound = new THREE.Audio(vars.listener);
+									// load a sound and set it as the Audio object's buffer
+									vars.audioLoader = new THREE.AudioLoader();
+
+
+
+									let elem = document.querySelector('#loading');
+									elem.parentNode.removeChild(elem);
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+
+		// ajout des controles
+		vars.controls = new OrbitControls(vars.camera, vars.renderer.domElement);
+
+		vars.controls.target.set(0, 100, 0);
+		vars.controls.update();
+
+		window.addEventListener('resize', Scene.onWindowResize, false);
+		window.addEventListener('mousemove', Scene.onMouseMove, false);
+
+		vars.stats = new Stats();
+		vars.container.appendChild(vars.stats.dom);
+
+		Scene.animate();
+	}
+};
+
+Scene.init();
